494e008fb3a62493fcd433d479fcdb8a9067ff26
David Kornel <kornys@outlook.com>
2024-02-27 11:21:41 +0100

    Create local kubeconfig also for kubectl
    Signed-off-by: David Kornel <kornys@outlook.com>

--- test-frame-common/src/main/java/io/skodjob/testframe/clients/KubeClient.java
+++ test-frame-common/src/main/java/io/skodjob/testframe/clients/KubeClient.java
@@ -9,6 +9,7 @@ import io.fabric8.kubernetes.client.ConfigBuilder;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClientBuilder;
 import io.fabric8.openshift.client.OpenShiftClient;
+import io.skodjob.testframe.TestFrameConstants;
 import io.skodjob.testframe.TestFrameEnv;
 import io.skodjob.testframe.executor.Exec;
 
@@ -43,7 +44,7 @@ public class KubeClient {
         if (TestFrameEnv.KUBE_USERNAME != null
                 && TestFrameEnv.KUBE_PASSWORD != null
                 && TestFrameEnv.KUBE_URL != null) {
-            kubeconfigPath = createLocalKubeconfig(TestFrameEnv.KUBE_USERNAME, TestFrameEnv.KUBE_PASSWORD, TestFrameEnv.KUBE_URL);
+            kubeconfigPath = createLocalKubeconfig();
             return new ConfigBuilder()
                     .withUsername(TestFrameEnv.KUBE_USERNAME)
                     .withPassword(TestFrameEnv.KUBE_PASSWORD)
@@ -53,7 +54,7 @@ public class KubeClient {
                     .build();
         } else if (TestFrameEnv.KUBE_URL != null
                 && TestFrameEnv.KUBE_TOKEN != null) {
-            kubeconfigPath = createLocalKubeconfig(TestFrameEnv.KUBE_TOKEN, TestFrameEnv.KUBE_URL);
+            kubeconfigPath = createLocalKubeconfig();
             return new ConfigBuilder()
                     .withOauthToken(TestFrameEnv.KUBE_TOKEN)
                     .withMasterUrl(TestFrameEnv.KUBE_URL)
@@ -66,28 +67,81 @@ public class KubeClient {
         }
     }
 
-    private String createLocalKubeconfig(String username, String password, String apiUrl) {
+    private String createLocalKubeconfig() {
         try {
-            Exec.exec(null, Arrays.asList("oc", "login", "-u", username,
-                    "-p", password,
-                    "--insecure-skip-tls-verify",
-                    "--kubeconfig", TestFrameEnv.USER_PATH + "/test.kubeconfig",
-                    apiUrl), 0, false, true);
+            if (TestFrameEnv.CLIENT_TYPE.equals(TestFrameConstants.OPENSHIFT_CLIENT)) {
+                if (TestFrameEnv.KUBE_URL != null && TestFrameEnv.KUBE_TOKEN != null) {
+                    createLocalOcKubeconfig(TestFrameEnv.KUBE_TOKEN, TestFrameEnv.KUBE_URL);
+                } else {
+                    createLocalOcKubeconfig(TestFrameEnv.KUBE_USERNAME, TestFrameEnv.KUBE_PASSWORD, TestFrameEnv.KUBE_URL);
+                }
+            } else {
+                if (TestFrameEnv.KUBE_URL != null && TestFrameEnv.KUBE_TOKEN != null) {
+                    createLocalKubectlContext(TestFrameEnv.KUBE_TOKEN, TestFrameEnv.KUBE_URL);
+                } else {
+                    createLocalKubectlContext(TestFrameEnv.KUBE_USERNAME, TestFrameEnv.KUBE_PASSWORD, TestFrameEnv.KUBE_URL);
+                }
+            }
             return TestFrameEnv.USER_PATH + "/test.kubeconfig";
         } catch (Exception ex) {
             return null;
         }
     }
 
-    private String createLocalKubeconfig(String token, String apiUrl) {
-        try {
-            Exec.exec(null, Arrays.asList("oc", "login", "--token", token,
-                    "--insecure-skip-tls-verify",
-                    "--kubeconfig", TestFrameEnv.USER_PATH + "/test.kubeconfig",
-                    apiUrl), 0, false, true);
-            return TestFrameEnv.USER_PATH + "/test.kubeconfig";
-        } catch (Exception ex) {
-            return null;
-        }
+    private void createLocalOcKubeconfig(String username, String password, String apiUrl) {
+        Exec.exec(null, Arrays.asList("oc", "login",
+                "-u", username,
+                "-p", password,
+                "--insecure-skip-tls-verify",
+                "--kubeconfig", TestFrameEnv.USER_PATH + "/test.kubeconfig",
+                apiUrl), 0, false, true);
+    }
+
+    private void createLocalOcKubeconfig(String token, String apiUrl) {
+        Exec.exec(null, Arrays.asList("oc", "login", "--token", token,
+                "--insecure-skip-tls-verify",
+                "--kubeconfig", TestFrameEnv.USER_PATH + "/test.kubeconfig",
+                apiUrl), 0, false, true);
+    }
+
+    private void createLocalKubectlContext(String username, String password, String apiUrl) {
+
+        Exec.exec(null, Arrays.asList("kubectl", "config",
+                        "set-credentials", "test-user",
+                        "--username", username,
+                        "--password", password,
+                        "--kubeconfig", TestFrameEnv.USER_PATH + "/test.kubeconfig"),
+                0, false, true);
+        buildKubectlContext(apiUrl);
+
+    }
+
+    private void createLocalKubectlContext(String token, String apiUrl) {
+        Exec.exec(null, Arrays.asList("kubectl", "config",
+                        "set-credentials", "test-user",
+                        "--token", token,
+                        "--kubeconfig", TestFrameEnv.USER_PATH + "/test.kubeconfig"),
+                0, false, true);
+        buildKubectlContext(apiUrl);
+
+    }
+
+    private void buildKubectlContext(String apiUrl) {
+        Exec.exec(null, Arrays.asList("kubectl", "config",
+                        "set-cluster", "test-cluster",
+                        "--insecure-skip-tls-verify=true", "--server", apiUrl,
+                        "--kubeconfig", TestFrameEnv.USER_PATH + "/test.kubeconfig"),
+                0, false, true);
+        Exec.exec(null, Arrays.asList("kubectl", "config",
+                        "set-context", "test-context",
+                        "--user", "test-user",
+                        "--cluster", "test-cluster",
+                        "--namespace", "default",
+                        "--kubeconfig", TestFrameEnv.USER_PATH + "/test.kubeconfig"),
+                0, false, true);
+        Exec.exec(null, Arrays.asList("kubectl", "config",
+                        "use-context", "test-context",
+                        "--kubeconfig", TestFrameEnv.USER_PATH + "/test.kubeconfig"),
+                0, false, true);
     }
 }
