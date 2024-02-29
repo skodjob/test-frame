3ccd70c84171f3d2e6d7dd4da117d6871e8e7f00
David Kornel <kornys@outlook.com>
2024-02-29 09:54:32 +0100

    Add RM annotations for better UX
    Signed-off-by: David Kornel <kornys@outlook.com>

---
+++ test-frame-common/src/main/java/io/skodjob/testframe/listeners/ResourceManagerCleanerExtension.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright Skodjob authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+package io.skodjob.testframe.listeners;
+
+import io.skodjob.testframe.resources.ResourceManager;
+import org.junit.jupiter.api.extension.AfterAllCallback;
+import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+/**
+ * jUnit5 specific class which listening on test callbacks
+ */
+public class ResourceManagerCleanerExtension
+        implements AfterAllCallback, AfterEachCallback {
+
+    @Override
+    public void afterAll(ExtensionContext extensionContext) {
+        ResourceManager.setTestContext(extensionContext);
+        ResourceManager.getInstance().deleteResources();
+    }
+
+    @Override
+    public void afterEach(ExtensionContext extensionContext) {
+        ResourceManager.setTestContext(extensionContext);
+        ResourceManager.getInstance().deleteResources();
+    }
+}
