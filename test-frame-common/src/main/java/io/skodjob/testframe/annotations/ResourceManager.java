c259c0219160253795b51838aa6bf5cf63bc8617
David Kornel <kornys@outlook.com>
2024-02-29 10:08:05 +0100

    Add RM annotations for better UX (#19)
    * Add RM annotations for better UX
    
    Signed-off-by: David Kornel <kornys@outlook.com>

---
+++ test-frame-common/src/main/java/io/skodjob/testframe/annotations/ResourceManager.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright Skodjob authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+package io.skodjob.testframe.annotations;
+
+import io.skodjob.testframe.listeners.ResourceManagerCleanerExtension;
+import io.skodjob.testframe.listeners.ResourceManagerExtension;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target(ElementType.TYPE)
+@Retention(RUNTIME)
+@ExtendWith(ResourceManagerExtension.class)
+@ExtendWith(ResourceManagerCleanerExtension.class)
+public @interface ResourceManager {
+}
---
+++ test-frame-common/src/main/java/io/skodjob/testframe/annotations/ResourceManagerDebug.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright Skodjob authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+package io.skodjob.testframe.annotations;
+
+import io.skodjob.testframe.listeners.ResourceManagerExtension;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target(ElementType.TYPE)
+@Retention(RUNTIME)
+@ExtendWith(ResourceManagerExtension.class)
+public @interface ResourceManagerDebug {
+}
