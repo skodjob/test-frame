f3deb2177c9cbd1222dd46cb207997daaf21a3f2
Lukáš Král <53821852+im-konge@users.noreply.github.com>
2024-05-14 11:13:20 +0200

    Create TestEnvironmentVariables object for storing the env vars in each test suite (#62)
    * Create TestEnvironmentVariables object for storing the env vars in each test suite
    
    Signed-off-by: Lukas Kral <lukywill16@gmail.com>
    
    public for getOrDefault
    
    add unit test
    
    Signed-off-by: Lukas Kral <lukywill16@gmail.com>
    
    * changes after rebase
    
    ---------
    
    Signed-off-by: Lukas Kral <lukywill16@gmail.com>

--- test-frame-common/src/main/java/io/skodjob/testframe/TestFrameEnv.java
+++ test-frame-common/src/main/java/io/skodjob/testframe/TestFrameEnv.java
@@ -4,30 +4,15 @@
  */
 package io.skodjob.testframe;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.nio.file.Paths;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
-import java.util.function.Function;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.yaml.snakeyaml.Yaml;
+import io.skodjob.testframe.environment.TestEnvironmentVariables;
 
 /**
  * Class which holds environment variables for system tests.
  */
 public final class TestFrameEnv {
 
-    private static final Logger LOGGER = LogManager.getLogger(TestFrameEnv.class);
-    private static final Map<String, String> VALUES = new HashMap<>();
-    private static final Map<String, Object> YAML_DATA = loadConfigurationFile();
+    private static final TestEnvironmentVariables ENV_VARIABLES = new TestEnvironmentVariables();
 
-    private static final String CONFIG_FILE_PATH_ENV = "ENV_FILE";
     private static final String CLIENT_TYPE_ENV = "CLIENT_TYPE";
     private static final String TOKEN_ENV = "KUBE_TOKEN";
     private static final String URL_ENV = "KUBE_URL";
@@ -39,72 +24,25 @@ public final class TestFrameEnv {
     /**
      * The type of client.
      */
-    public static final String CLIENT_TYPE = getOrDefault(CLIENT_TYPE_ENV, TestFrameConstants.KUBERNETES_CLIENT);
+    public static final String CLIENT_TYPE =
+        ENV_VARIABLES.getOrDefault(CLIENT_TYPE_ENV, TestFrameConstants.KUBERNETES_CLIENT);
 
 
     /**
      * The token for accessing the Kubernetes cluster.
      */
-    public static final String KUBE_TOKEN = getOrDefault(TOKEN_ENV, null);
+    public static final String KUBE_TOKEN = ENV_VARIABLES.getOrDefault(TOKEN_ENV, null);
 
     /**
      * The URL for accessing the Kubernetes cluster.
      */
-    public static final String KUBE_URL = getOrDefault(URL_ENV, null);
+    public static final String KUBE_URL = ENV_VARIABLES.getOrDefault(URL_ENV, null);
 
     private TestFrameEnv() {
         // Private constructor to prevent instantiation
     }
 
     static {
-        String debugFormat = "{}: {}";
-        LoggerUtils.logSeparator("-", 30);
-        LOGGER.info("Used environment variables:");
-        VALUES.entrySet().stream()
-                .sorted(Map.Entry.comparingByKey())
-                .forEach(entry -> {
-                    if (!Objects.equals(entry.getValue(), "null")) {
-                        LOGGER.info(debugFormat, entry.getKey(), entry.getValue());
-                    }
-                });
-        LoggerUtils.logSeparator("-", 30);
-    }
-
-    /**
-     * Print method.
-     */
-    public static void print() {
-        // Method implementation
-    }
-
-    private static String getOrDefault(String varName, String defaultValue) {
-        return getOrDefault(varName, String::toString, defaultValue);
-    }
-
-    private static <T> T getOrDefault(String var, Function<String, T> converter, T defaultValue) {
-        String value = System.getenv(var) != null ?
-                System.getenv(var) :
-                (Objects.requireNonNull(YAML_DATA).get(var) != null ?
-                        YAML_DATA.get(var).toString() :
-                        null);
-        T returnValue = defaultValue;
-        if (value != null) {
-            returnValue = converter.apply(value);
-        }
-        VALUES.put(var, String.valueOf(returnValue));
-        return returnValue;
-    }
-
-    private static Map<String, Object> loadConfigurationFile() {
-        String config = System.getenv().getOrDefault(CONFIG_FILE_PATH_ENV,
-                Paths.get(System.getProperty("user.dir"), "config.yaml").toAbsolutePath().toString());
-        Yaml yaml = new Yaml();
-        try {
-            File yamlFile = new File(config).getAbsoluteFile();
-            return yaml.load(new FileInputStream(yamlFile));
-        } catch (IOException ex) {
-            LOGGER.info("Yaml configuration not provided or does not exist");
-            return Collections.emptyMap();
-        }
+        ENV_VARIABLES.logEnvironmentVariables();
     }
 }
