3508591a08bcbd6b898466cb01a0b6fe3e5b137c
David Kornel <kornys@outlook.com>
2024-05-16 11:47:17 +0200

    Reimplement TFConstants to interface instead of class (#75)
    Signed-off-by: David Kornel <kornys@outlook.com>

--- test-frame-common/src/main/java/io/skodjob/testframe/TestFrameConstants.java
+++ test-frame-common/src/main/java/io/skodjob/testframe/TestFrameConstants.java
@@ -9,54 +9,51 @@ import java.time.Duration;
 /**
  * Constants used in the test framework.
  */
-public final class TestFrameConstants {
-    private TestFrameConstants() {
-        // Private constructor to prevent instantiation
-    }
+public interface TestFrameConstants {
 
     /**
      * Global poll interval in milliseconds (long).
      */
-    public static final long GLOBAL_POLL_INTERVAL_LONG = Duration.ofSeconds(15).toMillis();
+    long GLOBAL_POLL_INTERVAL_LONG = Duration.ofSeconds(15).toMillis();
 
     /**
      * Global poll interval in milliseconds (medium).
      */
-    public static final long GLOBAL_POLL_INTERVAL_MEDIUM = Duration.ofSeconds(10).toMillis();
+    long GLOBAL_POLL_INTERVAL_MEDIUM = Duration.ofSeconds(10).toMillis();
 
     /**
      * Global poll interval in milliseconds (short).
      */
-    public static final long GLOBAL_POLL_INTERVAL_SHORT = Duration.ofSeconds(5).toMillis();
+    long GLOBAL_POLL_INTERVAL_SHORT = Duration.ofSeconds(5).toMillis();
 
     /**
      * Global poll interval in milliseconds (1 second).
      */
-    public static final long GLOBAL_POLL_INTERVAL_1_SEC = Duration.ofSeconds(1).toMillis();
+    long GLOBAL_POLL_INTERVAL_1_SEC = Duration.ofSeconds(1).toMillis();
 
     /**
      * Global timeout in milliseconds (medium).
      */
-    public static final long GLOBAL_TIMEOUT_MEDIUM = Duration.ofMinutes(5).toMillis();
+    long GLOBAL_TIMEOUT_MEDIUM = Duration.ofMinutes(5).toMillis();
 
 
     /**
      * Global timeout in milliseconds.
      */
-    public static final long GLOBAL_TIMEOUT = Duration.ofMinutes(10).toMillis();
+    long GLOBAL_TIMEOUT = Duration.ofMinutes(10).toMillis();
 
     /**
      * Stability timeout in milliseconds
      */
-    public static final long GLOBAL_STABILITY_TIME = Duration.ofMinutes(1).toMillis();
+    long GLOBAL_STABILITY_TIME = Duration.ofMinutes(1).toMillis();
 
     /**
      * OpenShift client type.
      */
-    public static final String OPENSHIFT_CLIENT = "oc";
+    String OPENSHIFT_CLIENT = "oc";
 
     /**
      * Kubernetes client type.
      */
-    public static final String KUBERNETES_CLIENT = "kubectl";
+    String KUBERNETES_CLIENT = "kubectl";
 }
