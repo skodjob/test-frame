4ec9c10a175cb057a18ef48fd6d850f9e1c0dc4e
David Kornel <kornys@outlook.com>
2024-05-31 09:03:43 +0200

    Add maven wrapper (#91)
    Signed-off-by: David Kornel <kornys@outlook.com>

--- .github/scripts/push-to-nexus.sh
+++ .github/scripts/push-to-nexus.sh
@@ -5,7 +5,7 @@ export GPG_TTY=$(tty)
 echo $GPG_SIGNING_KEY | base64 -d > signing.gpg
 gpg --batch --import signing.gpg
 
-GPG_EXECUTABLE=gpg mvn -DskipTests -s ./.github/scripts/settings.xml -P ossrh clean deploy
+GPG_EXECUTABLE=gpg ./mvnw -DskipTests -s ./.github/scripts/settings.xml -P ossrh clean deploy
 
 rm -rf signing.gpg
 gpg --delete-keys
