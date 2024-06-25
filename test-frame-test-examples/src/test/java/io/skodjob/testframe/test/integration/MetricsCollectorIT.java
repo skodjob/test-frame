c9a460d3bda38ff3a69ef13e450001058aa3d7ca
David Kornel <kornys@outlook.com>
2024-06-25 12:07:10 +0200

    Add basic example test for MetricsCollector
    Signed-off-by: David Kornel <kornys@outlook.com>

---
+++ test-frame-test-examples/src/test/java/io/skodjob/testframe/test/integration/MetricsCollectorIT.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright Skodjob authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+package io.skodjob.testframe.test.integration;
+
+import io.fabric8.kubernetes.api.model.HasMetadata;
+import io.fabric8.kubernetes.api.model.LabelSelector;
+import io.fabric8.kubernetes.api.model.LabelSelectorBuilder;
+import io.skodjob.testframe.MetricsCollector;
+import io.skodjob.testframe.MetricsComponent;
+import io.skodjob.testframe.resources.KubeResourceManager;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public final class MetricsCollectorIT extends AbstractIT {
+
+    @Test
+    void testCollectMetrics() throws IOException {
+        //Create deployment
+        List<HasMetadata> resources = KubeResourceManager.getKubeClient()
+                .readResourcesFromFile(getClass().getClassLoader().getResourceAsStream("metrics-example.yaml"));
+
+        KubeResourceManager.getInstance().createResourceWithWait(resources.toArray(new HasMetadata[0]));
+
+        // Check deployment is not null
+        assertNotNull(KubeResourceManager.getKubeClient().getClient().namespaces().withName("metrics-test").get());
+        assertNotNull(KubeResourceManager.getKubeClient().getClient().apps().deployments()
+                .inNamespace("metrics-test").withName("prometheus-example").get());
+        assertNotNull(KubeResourceManager.getKubeClient().getClient().apps().deployments()
+                .inNamespace("metrics-test").withName("scraper-pod").get());
+
+        // Create metrics collector
+        MetricsCollector collector = new MetricsCollector.Builder()
+                .withNamespaceName("metrics-test")
+                .withScraperPodName(KubeResourceManager.getKubeClient()
+                        .listPodsByPrefixInName("metrics-test", "scraper-pod").get(0)
+                        .getMetadata().getName())
+                .withComponent(new MetricsComponent() {
+                    public int getDefaultMetricsPort() { return 8080; }
+                    public String getDefaultMetricsPath() { return "/metrics"; }
+                    public LabelSelector getLabelSelector() { return new LabelSelectorBuilder()
+                            .withMatchLabels(Map.of("app", "prometheus-example-app"))
+                            .build();
+                    }
+                })
+                .build();
+
+
+        assertDoesNotThrow(() -> collector.collectMetricsFromPods(30000)); // timeout in milliseconds
+        Map<String, String> metrics = collector.getCollectedData();
+        assertTrue(metrics.containsKey(KubeResourceManager.getKubeClient()
+                .listPodsByPrefixInName("metrics-test", "prometheus-example").get(0)
+                .getMetadata().getName()));
+    }
+}
