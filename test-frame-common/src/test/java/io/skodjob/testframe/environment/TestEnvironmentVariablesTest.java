88698d4ef435d7077e7da630b0fe949fced308f4
David Kornel <kornys@outlook.com>
2024-07-25 17:09:11 +0200

    Add unit tests
    Signed-off-by: David Kornel <kornys@outlook.com>

--- test-frame-common/src/test/java/io/skodjob/testframe/environment/TestEnvironmentVariablesTest.java
+++ test-frame-common/src/test/java/io/skodjob/testframe/environment/TestEnvironmentVariablesTest.java
@@ -2,9 +2,8 @@
  * Copyright Skodjob authors.
  * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
  */
-package io.skodjob.testframe.clients;
+package io.skodjob.testframe.environment;
 
-import io.skodjob.testframe.environment.TestEnvironmentVariables;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
