6ee3bc84de77ecd135b8e85fb01199c3e75a80c0
David Kornel <kornys@outlook.com>
2024-08-20 11:48:15 +0200

    Add info into TVS about TestClass started/finished (#170)
    ## Description
    
    Added aslo separation on test class level to see thich beforeall and
    afterall is for what class.
    
    
    ## Type of Change
    
    Please delete options that are not relevant.
    
    * New feature (non-breaking change which adds functionality)
    
    ## Checklist
    
    - [x] My code follows the style guidelines of this project
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas
    - [x] I have made corresponding changes to the documentation
    - [x] My changes generate no new warnings
    - [x] I have added tests that prove my fix is effective or that my
    feature works
    - [x] New and existing unit/integration tests pass locally with my
    changes
    
    Signed-off-by: David Kornel <kornys@outlook.com>

--- test-frame-common/src/main/java/io/skodjob/testframe/listeners/TestVisualSeparatorExtension.java
+++ test-frame-common/src/main/java/io/skodjob/testframe/listeners/TestVisualSeparatorExtension.java
@@ -5,7 +5,9 @@
 package io.skodjob.testframe.listeners;
 
 import io.skodjob.testframe.utils.LoggerUtils;
+import org.junit.jupiter.api.extension.AfterAllCallback;
 import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.BeforeEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.slf4j.Logger;
@@ -14,7 +16,8 @@ import org.slf4j.LoggerFactory;
 /**
  * jUnit5 specific class which listening on test callbacks
  */
-public class TestVisualSeparatorExtension implements BeforeEachCallback, AfterEachCallback {
+public class TestVisualSeparatorExtension implements BeforeEachCallback, AfterEachCallback,
+    BeforeAllCallback, AfterAllCallback {
     private final Logger logger = LoggerFactory.getLogger(TestVisualSeparatorExtension.class);
 
     private TestVisualSeparatorExtension() {
@@ -22,15 +25,27 @@ public class TestVisualSeparatorExtension implements BeforeEachCallback, AfterEa
     }
 
     @Override
+    public void beforeAll(ExtensionContext extensionContext) {
+        LoggerUtils.logSeparator();
+        logger.info("TestClass {} STARTED", extensionContext.getRequiredTestClass().getName());
+    }
+
+    @Override
+    public void afterAll(ExtensionContext extensionContext) {
+        logger.info("TestClass {} FINISHED", extensionContext.getRequiredTestClass().getName());
+        LoggerUtils.logSeparator();
+    }
+
+    @Override
     public void beforeEach(ExtensionContext extensionContext) {
         LoggerUtils.logSeparator();
-        logger.info("{}.{}-STARTED", extensionContext.getRequiredTestClass().getName(),
+        logger.info("Test {}.{} STARTED", extensionContext.getRequiredTestClass().getName(),
             extensionContext.getDisplayName().replace("()", ""));
     }
 
     @Override
     public void afterEach(ExtensionContext extensionContext) {
-        logger.info("{}.{}-FINISHED", extensionContext.getRequiredTestClass().getName(),
+        logger.info("Test {}.{} FINISHED", extensionContext.getRequiredTestClass().getName(),
             extensionContext.getDisplayName().replace("()", ""));
         LoggerUtils.logSeparator();
     }
