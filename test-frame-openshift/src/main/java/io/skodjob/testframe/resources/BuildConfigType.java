0efd20b9728717f45dd1aa4e28889767ffa26855
Lukas Kral <lukywill16@gmail.com>
2025-03-11 11:46:46 +0100

    Add BuildConfigType and ImageStreamType
    Signed-off-by: Lukas Kral <lukywill16@gmail.com>

---
+++ test-frame-openshift/src/main/java/io/skodjob/testframe/resources/BuildConfigType.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright Skodjob authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+package io.skodjob.testframe.resources;
+
+import io.fabric8.kubernetes.client.dsl.MixedOperation;
+import io.fabric8.kubernetes.client.dsl.NonNamespaceOperation;
+import io.fabric8.openshift.api.model.Build;
+import io.fabric8.openshift.api.model.BuildConfig;
+import io.fabric8.openshift.api.model.BuildConfigList;
+import io.fabric8.openshift.client.dsl.BuildConfigResource;
+import io.skodjob.testframe.interfaces.ResourceType;
+
+import java.util.function.Consumer;
+
+/**
+ * Implementation of ResourceType for handling the {@link BuildConfig} resource.
+ */
+public class BuildConfigType implements ResourceType<BuildConfig> {
+
+    private final MixedOperation<BuildConfig, BuildConfigList, BuildConfigResource<BuildConfig, Void, Build>> client;
+
+    /**
+     * {@link BuildConfig} constructor for initializing the client.
+     */
+    public BuildConfigType() {
+        this.client = KubeResourceManager.get().kubeClient().getOpenShiftClient().buildConfigs();
+    }
+
+    /**
+     * Returns {@link BuildConfig} client.
+     *
+     * @return  client for BuildConfigs
+     */
+    @Override
+    public NonNamespaceOperation<?, ?, ?> getClient() {
+        return client;
+    }
+
+    /**
+     * Returns kind of the resource - in this case {@link BuildConfig}
+     *
+     * @return  kind of resource
+     */
+    @Override
+    public String getKind() {
+        return "BuildConfig";
+    }
+
+    /**
+     * Creates specific {@link BuildConfig} resource
+     *
+     * @param resource {@link BuildConfig} resource
+     */
+    @Override
+    public void create(BuildConfig resource) {
+        client.inNamespace(resource.getMetadata().getNamespace()).resource(resource).create();
+    }
+
+    /**
+     * Updates specific {@link BuildConfig} resource
+     *
+     * @param resource {@link BuildConfig} resource that will be updated
+     */
+    @Override
+    public void update(BuildConfig resource) {
+        client.inNamespace(resource.getMetadata().getNamespace()).resource(resource).update();
+    }
+
+    /**
+     * Deletes {@link BuildConfig} resource from Namespace in current context
+     *
+     * @param resource {@link BuildConfig} resource that will be deleted
+     */
+    @Override
+    public void delete(BuildConfig resource) {
+        client.inNamespace(resource.getMetadata().getNamespace()).resource(resource).delete();
+    }
+
+    /**
+     * Replaces {@link BuildConfig} resource using {@link Consumer}
+     * from which is the current {@link BuildConfig} resource updated
+     *
+     * @param resource  {@link BuildConfig} resource that will be replaced
+     * @param editor    {@link Consumer} containing updates to the resource
+     */
+    @Override
+    public void replace(BuildConfig resource, Consumer<BuildConfig> editor) {
+        BuildConfig toBeReplaced = client.inNamespace(resource.getMetadata().getNamespace())
+            .withName(resource.getMetadata().getName()).get();
+        editor.accept(toBeReplaced);
+        update(toBeReplaced);
+    }
+
+    /**
+     * Waits for {@link BuildConfig} to be ready (created/running)
+     *
+     * @param resource resource
+     * @return result of the readiness check
+     */
+    @Override
+    public boolean isReady(BuildConfig resource) {
+        return client.inNamespace(resource.getMetadata().getNamespace())
+            .withName(resource.getMetadata().getName()).isReady();
+    }
+
+    /**
+     * Waits for {@link BuildConfig} to be deleted
+     *
+     * @param resource resource
+     * @return result of the deletion
+     */
+    @Override
+    public boolean isDeleted(BuildConfig resource) {
+        return resource == null;
+    }
+}
---
+++ test-frame-openshift/src/main/java/io/skodjob/testframe/resources/ImageStreamType.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright Skodjob authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+package io.skodjob.testframe.resources;
+
+import io.fabric8.kubernetes.client.dsl.MixedOperation;
+import io.fabric8.kubernetes.client.dsl.NonNamespaceOperation;
+import io.fabric8.kubernetes.client.dsl.Resource;
+import io.fabric8.openshift.api.model.ImageStream;
+import io.fabric8.openshift.api.model.ImageStreamList;
+import io.skodjob.testframe.interfaces.ResourceType;
+
+import java.util.function.Consumer;
+
+/**
+ * Implementation of ResourceType for handling the {@link ImageStream} resource.
+ */
+public class ImageStreamType implements ResourceType<ImageStream> {
+
+    private final MixedOperation<ImageStream, ImageStreamList, Resource<ImageStream>> client;
+
+    /**
+     * {@link ImageStream} constructor for initializing the client.
+     */
+    public ImageStreamType() {
+        this.client = KubeResourceManager.get().kubeClient().getOpenShiftClient().imageStreams();
+    }
+
+    /**
+     * Returns {@link ImageStream} client.
+     *
+     * @return  client for ImageStreams
+     */
+    @Override
+    public NonNamespaceOperation<?, ?, ?> getClient() {
+        return client;
+    }
+
+    /**
+     * Returns kind of the resource - in this case {@link ImageStream}
+     *
+     * @return  kind of resource
+     */
+    @Override
+    public String getKind() {
+        return "ImageStream";
+    }
+
+    /**
+     * Creates specific {@link ImageStream} resource
+     *
+     * @param resource {@link ImageStream} resource
+     */
+    @Override
+    public void create(ImageStream resource) {
+        client.inNamespace(resource.getMetadata().getNamespace()).resource(resource).create();
+    }
+
+    /**
+     * Updates specific {@link ImageStream} resource
+     *
+     * @param resource {@link ImageStream} resource that will be updated
+     */
+    @Override
+    public void update(ImageStream resource) {
+        client.inNamespace(resource.getMetadata().getNamespace()).resource(resource).update();
+    }
+
+    /**
+     * Deletes {@link ImageStream} resource from Namespace in current context
+     *
+     * @param resource {@link ImageStream} resource that will be deleted
+     */
+    @Override
+    public void delete(ImageStream resource) {
+        client.inNamespace(resource.getMetadata().getNamespace()).resource(resource).delete();
+    }
+
+    /**
+     * Replaces {@link ImageStream} resource using {@link Consumer}
+     * from which is the current {@link ImageStream} resource updated
+     *
+     * @param resource  {@link ImageStream} resource that will be replaced
+     * @param editor    {@link Consumer} containing updates to the resource
+     */
+    @Override
+    public void replace(ImageStream resource, Consumer<ImageStream> editor) {
+        ImageStream toBeReplaced = client.inNamespace(resource.getMetadata().getNamespace())
+            .withName(resource.getMetadata().getName()).get();
+        editor.accept(toBeReplaced);
+        update(toBeReplaced);
+    }
+
+    /**
+     * Waits for {@link ImageStream} to be ready (created/running)
+     *
+     * @param resource resource
+     * @return result of the readiness check
+     */
+    @Override
+    public boolean isReady(ImageStream resource) {
+        return client.inNamespace(resource.getMetadata().getNamespace())
+            .withName(resource.getMetadata().getName()).isReady();
+    }
+
+    /**
+     * Waits for {@link ImageStream} to be deleted
+     *
+     * @param resource resource
+     * @return result of the deletion
+     */
+    @Override
+    public boolean isDeleted(ImageStream resource) {
+        return resource == null;
+    }
+}
