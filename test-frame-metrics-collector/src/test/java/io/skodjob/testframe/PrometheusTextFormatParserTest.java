f17fa5f893466c0e9b97b2a89c33570123ac1ff4
see-quick <maros.orsak159@gmail.com>
2025-04-07 12:00:32 +0200

    update another test
    Signed-off-by: see-quick <maros.orsak159@gmail.com>

--- test-frame-metrics-collector/src/test/java/io/skodjob/testframe/PrometheusTextFormatParserTest.java
+++ test-frame-metrics-collector/src/test/java/io/skodjob/testframe/PrometheusTextFormatParserTest.java
@@ -110,15 +110,13 @@ public class PrometheusTextFormatParserTest {
     }
 
     @Test
-    void testParseSumWithTrailingComma() throws IOException {
-        String data = "# TYPE strimzi_describe_topics_duration_seconds_sum gauge\n" +
-            "strimzi_describe_topics_duration_seconds_sum{kind=\"KafkaTopic\",namespace=\"co-namespace\"," +
-            "selector=\"strimzi.io/cluster=cluster-5e6e237c\",} 0.864833836\n";
+    void testParseGaugeSumWithTrailingComma() throws IOException {
+        String data = "# TYPE custom_operation_duration_seconds_sum gauge\n" +
+            "custom_operation_duration_seconds_sum{operation=\"write\",source=\"serviceA\",} 1.23456789\n";
         List<Metric> metrics = PrometheusTextFormatParser.parse(data);
-        // The orphaned _sum metric is parsed as a Gauge.
         assertEquals(1, metrics.size());
         Metric m = metrics.get(0);
         assertTrue(m instanceof Gauge);
-        assertEquals(0.864833836, ((Gauge) m).getValue(), 0.000001);
+        assertEquals(1.23456789, ((Gauge) m).getValue(), 0.000001);
     }
 }
