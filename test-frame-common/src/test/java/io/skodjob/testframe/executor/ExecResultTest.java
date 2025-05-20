17683f770e6e7a504529fe14d5e4e1c3e8f1bb5b
David Kornel <kornys@outlook.com>
2025-05-20 12:25:14 +0200

    Add JobUtilsTests and tests for executor
    Signed-off-by: David Kornel <kornys@outlook.com>

--- test-frame-common/src/test/java/io/skodjob/testframe/executor/ExecResultTest.java
+++ test-frame-common/src/test/java/io/skodjob/testframe/executor/ExecResultTest.java
@@ -10,7 +10,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class ExecResultTest {
+class ExecResultTest {
 
     @Test
     void testExitStatusSuccess() {
