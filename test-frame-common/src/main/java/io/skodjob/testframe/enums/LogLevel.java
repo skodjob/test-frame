4f2822cd7b499537048852a7d81f4ef4acf87bf4
Lukáš Král <53821852+im-konge@users.noreply.github.com>
2025-06-03 09:06:21 +0200

    Add possibility to specify log level in exec commands (#295)
    ## Description
    
    In one of the previous PRs the option to enable/disable logging to
    output when the `exec` command is executed.
    But in some cases - like commands generating long output - we want to
    log it, but on different level - for example just for debug in case that
    there is some failure, so we have more logs to check.
    
    This PR adds possibility to specify log level in the `exec` commands.
    For better usage (without need of adding the `slf4j` library directly
    into the code) I created the `LogLevel` enum, which contains all
    possible log levels and then it returns the appropriate `Level` enum
    from sl4j, so it can be used in the `LOGGER.atLevel()` method.
    
    ## Type of Change
    
    * New feature (non-breaking change which adds functionality)
    
    ## Checklist
    
    - [x] My code follows the style guidelines of this project
    - [x] I have performed a self-review of my own code
    - [x] I have commented my code, particularly in hard-to-understand areas
    - [x] I have made corresponding changes to the documentation
    - [x] My changes generate no new warnings
    - [x] I have added tests that prove my fix is effective or that my
    feature works
    - [x] New and existing unit/integration tests pass locally with my
    changes
    
    ---------
    
    Signed-off-by: Lukas Kral <lukywill16@gmail.com>

---
+++ test-frame-common/src/main/java/io/skodjob/testframe/enums/LogLevel.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright Skodjob authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+package io.skodjob.testframe.enums;
+
+import org.slf4j.event.Level;
+
+/**
+ * Enum class capturing available log levels that can be used in Test-Frame.
+ */
+public enum LogLevel {
+    /**
+     * Debug log level
+     */
+    DEBUG,
+    /**
+     * Info log level
+     */
+    INFO,
+    /**
+     * Warn log level
+     */
+    WARN,
+    /**
+     * Error log level
+     */
+    ERROR,
+    /**
+     * Trace log level
+     */
+    TRACE;
+
+    /**
+     * Based on {@param level} returns corresponding slf4j's log level (as {@link Level}.
+     *
+     * @param level     Desired log level.
+     *
+     * @return  based on {@param level} returns corresponding slf4j's log level (as {@link Level}.
+     */
+    public static Level logLevelToLevel(LogLevel level) {
+        switch (level) {
+            case DEBUG: return Level.DEBUG;
+            case WARN: return Level.WARN;
+            case ERROR: return Level.ERROR;
+            case TRACE: return Level.TRACE;
+            default: return Level.INFO;
+        }
+    }
+}
