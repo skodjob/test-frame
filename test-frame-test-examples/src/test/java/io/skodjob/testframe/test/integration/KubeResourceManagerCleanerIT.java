0de9d63e4d6b6f9dd7aa51638f85b0b67fe3f94c
David Kornel <kornys@outlook.com>
2025-06-26 16:56:13 +0200

    Fix async deletion of stored resources in KRM (#303)
    ## Description
    
    Fix for async deletion issue of stored resources caused by
    implementation of multi kube context feature
    
    
    ## Type of Change
    
    Please delete options that are not relevant.
    
    * Bug fix (non-breaking change which fixes an issue)
    
    ## Checklist
    
    - [ ] My code follows the style guidelines of this project
    - [ ] I have performed a self-review of my own code
    - [ ] I have commented my code, particularly in hard-to-understand areas
    - [ ] I have made corresponding changes to the documentation
    - [ ] My changes generate no new warnings
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] New and existing unit/integration tests pass locally with my
    changes
    
    Signed-off-by: David Kornel <kornys@outlook.com>

--- test-frame-test-examples/src/test/java/io/skodjob/testframe/test/integration/KubeResourceManagerCleanerIT.java
+++ test-frame-test-examples/src/test/java/io/skodjob/testframe/test/integration/KubeResourceManagerCleanerIT.java
@@ -27,7 +27,7 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-@ResourceManager(asyncDeletion = false)
+@ResourceManager(asyncDeletion = true)
 final class KubeResourceManagerCleanerIT extends AbstractIT {
 
     @BeforeAll
