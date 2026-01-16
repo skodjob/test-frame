f352272010313c0585aa7d1017102207d9442eac
David Kornel <kornys@outlook.com>
2026-01-16 12:14:33 +0100

    New module kubetest-junit as junit declarative extension
    Signed-off-by: David Kornel <kornys@outlook.com>

---
+++ kubetest-junit/src/main/java/io/skodjob/testframe/kubetest/annotations/InjectResourceManager.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright Skodjob authors.
+ * License: Apache License 2.0 (see the file LICENSE or http://apache.org/licenses/LICENSE-2.0.html).
+ */
+package io.skodjob.testframe.kubetest.annotations;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation to inject a KubeResourceManager instance into test methods or fields.
+ * The injected resource manager will be pre-configured with the test context
+ * and cleanup strategy.
+ * Usage:
+ * <pre>
+ * &#64;KubernetesTest
+ * class MyTest {
+ *     &#64;InjectResourceManager
+ *     KubeResourceManager resourceManager;
+ *
+ *     &#64;Test
+ *     void testWithResourceManager() {
+ *         resourceManager.createResourceWithWait(myDeployment);
+ *         // Resources will be automatically cleaned up
+ *     }
+ * }
+ * </pre>
+ */
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface InjectResourceManager {
+
+    /**
+     * The context to use for this resource manager.
+     * If not specified, uses the context from the test class annotation.
+     *
+     * @return cluster context name
+     */
+    String context() default "";
+}
